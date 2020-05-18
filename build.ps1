708aa36815dcd74fe3e160df2d82fc7f299234e3
alexbobylewa <alexbobylewa@gmail.com>
2020-05-18 15:57:17 +0300

    Go test

---
+++ build.ps1
@@ -0,0 +1,6 @@
+Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.PSVersion)"
+
+#Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { exit 1 }
+
+Write-Host "Congratulations! Your first script executed successfully"
+Start-Process -FilePath go -ArgumentList "build -i -ldflags -H=windowsgui $PSScriptRoot\main.go; .\\main.exe | out-string;"
