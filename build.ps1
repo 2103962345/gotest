bfec1d2067a777bd56cb6fe4e1d0e03ad7d2af53
2103962345 <2103962345@mail.ru>
2020-05-19 13:50:08 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -3,4 +3,5 @@ Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.
 #Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { exit 1 }
 
 Write-Host "Congratulations! Your first script executed successfully"
-Start-Process -FilePath go -ArgumentList "build $PSScriptRoot\main.go" -NoNewWindow
+Invoke-Expression "go build $PSScriptRoot\main.go"
+#Start-Process -FilePath go -ArgumentList "build $PSScriptRoot\main.go" -NoNewWindow
