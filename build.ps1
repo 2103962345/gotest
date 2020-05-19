76ed219ba2fc8880d37f1c66d5db6c8d9aa472e1
2103962345 <2103962345@mail.ru>
2020-05-19 17:26:25 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -3,5 +3,5 @@ Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.
 #Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { exit 1 }
 
 Write-Host "Congratulations! Your first script executed successfully"
-#Invoke-Expression "go build $PSScriptRoot\main.go"
-Start-Process -FilePath "C:\Go\bin\go.exe" -ArgumentList "version" -NoNewWindow
+Invoke-Expression "go build -i -ldflags -H=windowsgui main.go; .\\main.exe | out-string;"
+#Start-Process -FilePath "C:\Go\bin\go.exe" -ArgumentList "version" -NoNewWindow
