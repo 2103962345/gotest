714023314f852daf3dbdeb137e1d83659f7a845d
2103962345 <2103962345@mail.ru>
2020-05-19 13:40:04 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -3,4 +3,4 @@ Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.
 #Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { exit 1 }
 
 Write-Host "Congratulations! Your first script executed successfully"
-Start-Process -FilePath go -ArgumentList "build -i -ldflags -H=windowsgui $PSScriptRoot\main.go; .\\main.exe | out-string;"
+Start-Process -FilePath go -ArgumentList "build $PSScriptRoot\main.go"
